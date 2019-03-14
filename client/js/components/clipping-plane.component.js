4f9d2f9852fbd448b06f267cefcc4e18078a7ff5
Liam Wood <liamvwood@ufl.edu>
2019-03-13 21:38:51 -0400

    setting up clipping planes, broken

--- client/js/components/clipping-plane.component.js
+++ client/js/components/clipping-plane.component.js
@@ -1,3 +1,29 @@
-AFRAME.registerComponent('clipping-plane', function(){
-    
+AFRAME.registerComponent('clipping-plane', {
+    init: function () {
+        var scene = document.querySelector('a-scene')
+        this.marker = document.querySelector('#light-marker')
+        let plane = this
+        scene.addEventListener('render-target-loaded', function () {
+            // sceneEl.renderer is now set.
+            renderer = scene.renderer
+
+            // default normal of a plane is 0,0,1. Apply mesh rotation to it.
+            let clipPlane = new THREE.Plane().setFromNormalAndCoplanarPoint(
+                new THREE.Vector3(0, 1, 0), plane.el.object3D.position);
+            renderer.clippingPlanes = [clipPlane];
+        });
+
+    },
+    tick: function () {
+        if (this.marker.object3D.visible == true) {
+            // align clipping plane to scene.
+            this.renderer.clippingPlanes[0].setFromNormalAndCoplanarPoint(
+                new THREE.Vector3(0, 1, 0).applyQuaternion(sceneGroup.getWorldQuaternion()),
+                sceneGroup.getWorldPosition()
+            );
+        }
+        else {
+
+        }
+    }
 })
