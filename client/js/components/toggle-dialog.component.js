2e1fb07b56dc15fc794c31ce845e19a307a8f564
Liam Wood <liamvwood@ufl.edu>
2019-03-18 06:39:00 -0400

    push

---
+++ client/js/components/toggle-dialog.component.js
@@ -0,0 +1,10 @@
+AFRAME.registerComponent('toggle-dialog', {
+    init: function() {
+
+    },
+    tick: function() {
+        if (this.el.object3D.visible == true) {
+            
+        }
+    }
+})
--- client/js/controllers/ar.controller.js
+++ client/js/controllers/ar.controller.js
@@ -4,22 +4,30 @@ angular.module('harnApp').controller('ArController', function ($scope) {
     const kusama_marker = document.querySelector('#kusama-marker');
     const joyce_marker = document.querySelector('#joyce-marker');
 
-    const light_content = document.querySelector('#light-marker');
+    const light_content = document.querySelector('#light-content');
     const kusama_content = document.querySelector('#kusama-content');
     const joyce_content = document.querySelector('#joyce-content');
 
+    const light_text = document.querySelector('#light-text');
+    const kusama_text = document.querySelector('#kusama-text');
+    const joyce_text = document.querySelector('#joyce-text');
 
     center_button.addEventListener('click',function (){ 
     
         if (light_marker.object3D.visible == true) {
             console.log('light')
+            light_text.emit('light-fadeout')
+            light_content.emit('light-fadein')
         } 
         else if (kusama_marker.object3D.visible == true) {
             console.log('kusama')
-
+            kusama_text.emit('kusama-fadeout')
+            kusama_content.emit('kusama-fadein')
         }
         else if(joyce_marker.object3D.visible == true) {
             console.log('joyce')
+            joyce_text.emit('joyce-fadeout')
+            joyce_content.emit('joyce-fadein')
         }
     });
 
