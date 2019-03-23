d6b8831832faaf090b252782921e4db571200651
Liam Wood <liamvwood@ufl.edu>
2019-03-23 16:12:02 -0400

    refactoring and clean up

--- client/js/components/vidhandler.js
+++ client/js/components/vidhandler.js
@@ -1,7 +1,7 @@
 AFRAME.registerComponent('vidhandler', { 
     // ... 
     init: function () { 
-        this.video = document.querySelector("#Kusama");
+        this.video = document.querySelector("#kusama-video");
         this.playing = true;
         this.video.play();
         document.querySelector("#Kusama").play()
