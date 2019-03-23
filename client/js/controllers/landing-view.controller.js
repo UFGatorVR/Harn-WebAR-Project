46070d250ec48b3b6f25fa32f2d20d2bd9ad98ed
Kelvin-Chang <kelvinchang36@gmail.com>
2019-03-23 15:35:57 -0400

    broken stuff

--- client/js/controllers/landing-view.controller.js
+++ client/js/controllers/landing-view.controller.js
@@ -1,3 +1,16 @@
-angular.module('harnApp').controller('LandingController', function($http, $scope) {
-    
+angular.module('harnApp').controller('LandingController', function ($http, $scope) {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
 })
